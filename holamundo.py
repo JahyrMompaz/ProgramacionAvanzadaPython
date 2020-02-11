b0adf564f3a34840dec10489d89c2bad3682308d
FelipeRamirezPhD <feliperamirezphd@gmail.com>
2020-02-11 06:44:20 +0000

    New Programs

---
+++ holamundo.py
@@ -0,0 +1,10 @@
+# Programa 01
+# holamundo típico. Muestra un simple mensaje, para ilustrar la estructrura
+# típica de un programa.
+
+# El punto de entrada se llamará main
+# Se compone de un estatuto def
+
+def main():
+  print("Hola mundo, ahora en Python")
+
---
+++ muestravariable.py
@@ -0,0 +1,9 @@
+# Progrma 02
+# Declara una variable, y la muestra.
+
+def main():
+  # En python se declara una variable por asignacion. El datatype depende
+  # del valor que se asigna.
+  txt="Hola mundo, con variable"
+  print(txt)
+  
