90cb53260e062ae8e02f8083192732fee2759262
JahyrMompaz <61512755+JahyrMompaz@users.noreply.github.com>
2020-02-27 16:39:29 -0600

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-# Programa 00
-# Programa principal de repl.it: todos los programas se ejecutan como
-# función de este progama,
-# Referir de la siguiente manera:
-# from nombreprogramasinextensiónpy import nombrefunciónprincipaldelprograma
-# nombrefunciónprincipaldelprograma()   -> Le llamaremos main(), para 
-# homologar con otros lenguajes de programación, como C#
-
 from funciones import main
 main()
